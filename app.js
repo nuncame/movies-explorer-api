f645b554fb9fbac8a8d5de47e69604557521904a
Victoria Markova <vii.nuncame@gmail.com>
2023-08-29 17:38:19 +0300

    fix: updates for level-3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
