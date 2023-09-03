4690f4114377478cd4564488347428c7654bf522
Victoria Markova <vii.nuncame@gmail.com>
2023-09-03 18:18:16 +0300

    fix: cors

--- app.js
+++ app.js
@@ -29,24 +29,9 @@ const allowedCors = [
 
 app.use(cors({
   origin: allowedCors,
+  methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
-
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   return next();
-// });
 
 app.use(requestLogger);
 
