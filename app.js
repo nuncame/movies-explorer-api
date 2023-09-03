ae84beff2ad7b6027d2397faf4af9ee89d3da0d1
Victoria Markova <vii.nuncame@gmail.com>
2023-09-03 15:06:25 +0300

    fix: cors

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./utils/limiter');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
-
-const allowedCors = [
-  'https://mymovies-nuncame.nomoreparties.co',
-  'http://mymovies-nuncame.nomoreparties.co',
-  'http://localhost:3000',
-];
 
 mongoose
   .connect(DB_URL, {
@@ -26,21 +21,32 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
+const allowedCors = [
+  'https://mymovies-nuncame.nomoreparties.co',
+  'http://mymovies-nuncame.nomoreparties.co',
+  'http://localhost:3001',
+];
+
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   return next();
+// });
 
 app.use(requestLogger);
 
